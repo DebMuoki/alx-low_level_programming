4bd40e09f986330d18e01f595943f2b7683dab6d
DebMuoki <ndanumuokideb@gmail.com>
2023-04-03 12:43:53 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,27 +7,17 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int k, n;
 
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (k = 0; s[k] != '\0'; k++)
 	{
-		check = 0;
-
 		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
+			if (accept[n] == s[k])
+				break;
 		}
-		if (check == 0)
-		{
+		if (accept[n] == '\0')
 			break;
-		}
-		value++;
 	}
-	return (value);
+	return (k);
 }
