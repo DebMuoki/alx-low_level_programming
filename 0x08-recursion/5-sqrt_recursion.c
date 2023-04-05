07829bba0fe6569b07c035808d6d0b58707ca7c0
DebMuoki <ndanumuokideb@gmail.com>
2023-04-05 13:33:51 -0700

    TASK SIX

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,4 @@
 #include "main.h"
+
 /**
-  * _sqrt_recursion - retursn the naturale squeroot of a number
-  * @n: number whose square root is being found
-  * Return: returns natural squareroot
-  */
-
-int _sqrt_recursion(int n)
-{
-	int i = 1;
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-
-	while (i * i < n)
-		i++;
-	return ((i * i ==n) ? i : -1);
-}
+  * 
