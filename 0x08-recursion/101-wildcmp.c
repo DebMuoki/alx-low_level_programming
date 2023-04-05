9afdf97c0b188e9c38b01bf22e2a70e4a6c0e7bc
DebMuoki <ndanumuokideb@gmail.com>
2023-04-05 16:05:03 -0700

    TASK EIGHT INCEPTION

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,33 +11,33 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-	if (*s2 == '\0' || *s2 == '*')
+	if (*s2 == '\0')
 	{
-            return (1);
-        }
-        else
-        {
-            return (0);
-        }
-    }
-    else if (*s2 == '*')
-    {
-        if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-        {
-            return (1);
-        }
-        else
-        {
-            return (0);
-        }
-    }
-    else if (*s1 == *s2)
-    {
-        return (wildcmp(s1 + 1, s2 + 1));
-    }
-    else
-    {
-        return (0);
-    }
+	return (1);
+	}
+	else if (*s2 == '*')
+	{
+	return (wildcmp(s1, s2 + 1));
+	}
+	else
+	{
+	return (0);
+	}
+	}
+	else if (*s2 == '\0')
+	{
+	return (0);
+	}
+	else if (*s2 == '*')
+	{
+	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	else if (*s1 == *s2)
+	{
+	return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else
+	{
+	return (0);
+	}
 }
-
