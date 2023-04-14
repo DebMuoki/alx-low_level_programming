1d526eb643794a074e5d09b73b6cee3f81001a6a
DebMuoki <ndanumuokideb@gmail.com>
2023-04-14 12:10:43 -0700

    TASK SIX

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,36 +14,40 @@ char **strtow(char *str)
 	char **words;
 	int i, j, k, len, word_count;
 
-	if (str == NULL || *str == '\0' || strspn(str, " ") == strlen(str))
-	return (NULL);
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	/* Check if the input string consists of only spaces */
+	if (strspn(str, " ") == strlen(str))
+		return (NULL);
 
 	len = strlen(str);
 	words = malloc((len / 2 + 2) * sizeof(char *));
 	if (words == NULL)
-	return (NULL);
+		return (NULL);
 
 	for (i = 0, word_count = 0; i < len; i++)
+	{
+		if (!isspace(str[i]) && (i == 0 || isspace(str[i - 1])))
 		{
-	if (!isspace(str[i]) && (i == 0 || isspace(str[i - 1])))
-	{
-	for (j = i; j < len && !isspace(str[j]); j++)
-		;
-	words[word_count] = malloc((j - i + 1) * sizeof(char));
-	if (words[word_count] == NULL)
-	{
-	while (--word_count >= 0)
-	free(words[word_count]);
-	free(words);
-	return (NULL);
-	}
-				for (k = 0; k < j - i; k++)
-					words[word_count][k] = str[i + k];
-				words[word_count][k] = '\0';
-				word_count++;
-				i = j;
-			}
+		for (j = i; j < len && !isspace(str[j]); j++)
+			;
+		words[word_count] = malloc((j - i + 1) * sizeof(char));
+		if (words[word_count] == NULL)
+		{
+			while (--word_count >= 0)
+			free(words[word_count]);
+			free(words);
+			return (NULL);
 		}
-		words[word_count] = NULL;
-		return (words);
+		for (k = 0; k < j - i; k++)
+			words[word_count][k] = str[i + k];
+		words[word_count][k] = '\0';
+		word_count++;
+		i = j;
+		}
+	}
+	words[word_count] = NULL;
+	return (words);
 }
 
