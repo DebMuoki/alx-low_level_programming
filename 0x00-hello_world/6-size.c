97c15026e3bb14670e3402ca313c5705d079fc62
DebMuoki <ndanumuokideb@gmail.com>
2023-04-15 10:45:54 -0700

    TASK SIX

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-
 /**
-  * main - main function
-  *
-  * Return: 0 on success
-  */
+ * main - entry point
+ * printf - prints formatted text to the console
+ * sizeof - returns the size in bytes of its operand	* Return: 0 for success
+ */
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-	printf("Size of int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)", sizeof(f));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of short: %d byte(s)\n", sizeof(short));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of double: %d byte(s)\n", sizeof(double));
+	printf("Size of long double: %d byte(s)\n", sizeof(long double));
+	printf("Size of void*: %d byte(s)\n", sizeof(void *));
 	return (0);
 }
