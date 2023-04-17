62b3e5dec15002fe73e0d6f051e95049524e97ab
DebMuoki <ndanumuokideb@gmail.com>
2023-04-17 03:46:00 -0700

    TASK TWO

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * __FILE__ - macro expands to name of current source file as a string
+  * Return: 0 if successful
+  */
+
+int main(void)
+{
+	printf("Compiled from: %s\n", __FILE__);
+	return (0);
+}
+
