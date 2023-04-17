231eeddb100f2f42bf808c779477d41e58defaa9
DebMuoki <ndanumuokideb@gmail.com>
2023-04-17 05:50:43 -0700

    TASK ZERO

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a struct representing a dog's name, age and owner
+  * @name: the name of the dog
+  * @age: age of the dog
+  * @owner: name of the dog's owner
+  */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
