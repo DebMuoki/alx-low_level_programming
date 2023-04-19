9b5dc306b4405a9489667c928f3ebbadfa108602
DebMuoki <ndanumuokideb@gmail.com>
2023-04-19 12:30:57 -0700

    TASK ONE

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Applies a function to each element of an array.
- * @array: Pointer to the array of integers.
- * @size: Size of the array.
- * @action: Pointer to the function to apply to each element.
+ * array_iterator - It applies a function to each element of an array.
+ * @array: It is a pointer to the array of integers.
+ * @size: the size of the array.
+ * @action: a pointer to the function to apply to each element.
  *
  * Return: void.
  */
