fd0bec7c0b8c711dfa5723eeb2b04dbd38915999
DebMuoki <ndanumuokideb@gmail.com>
2023-04-19 11:34:49 -0700

    TASK ZERO

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,13 @@
+#include "function_pointers.h"
+
 /**
- * print_name - Prints a given name using a provided function
+ * print_name - function that prints a given name using a provided function
  * @name: Pointer to the name to be printed
  * @f: Pointer to the function used to print the name
  *
  * Return: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 		if (!name || !f)
