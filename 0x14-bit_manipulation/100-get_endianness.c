cd66e2ff4ac07de225336580b3c0891dcf053309
DebMuoki <ndanumuokideb@gmail.com>
2023-05-04 10:52:42 -0700

    TASK six

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - function that checks endianness of the machine.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+	return (1);
+	else
+	return (0);
+}
+
