39d6bfb9bd96750d9c90937a75090ec40aa197ea
DebMuoki <ndanumuokideb@gmail.com>
2023-07-05 08:47:35 -0700

    Task two

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that counts the length of a string
+ * @s: the string
+ * Return: return value is the length of the string
+ **/
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
