647b983fe0c198d0c91cd253fdd484cab450ac03
DebMuoki <ndanumuokideb@gmail.com>
2023-07-07 11:50:47 -0700

    Task one and two

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the command-line arguments
- *
+ * main - This is the entry point of the program
+ * @argc: This is the number of command-line arguments
+ * @argv: This is an array of strings containing the command-line arguments
  * Return: 0 on success
  */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
