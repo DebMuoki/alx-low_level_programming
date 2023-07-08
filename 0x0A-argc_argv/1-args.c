3bdbcb5cb89477b68a59ada81d4e0fb8a0fe4101
DebMuoki <ndanumuokideb@gmail.com>
2023-07-08 05:02:51 -0700

    Task one

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: this is the number of arguments
+ * @argv: this is the array of arguments
+ * Return: the return value is void
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
