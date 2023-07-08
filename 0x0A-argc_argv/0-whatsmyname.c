a89d896f1ce15c93d2e905dbe18c570ea88ee120
DebMuoki <ndanumuokideb@gmail.com>
2023-07-08 04:51:19 -0700

    Task zero

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: this is the number of arguments
+ * @argv: this is the array of arguments
+ * Return: the return value is Zero
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
