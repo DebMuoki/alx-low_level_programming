7e23de14bf8c5618ea09762dba49e89066522857
DebMuoki <ndanumuokideb@gmail.com>
2023-07-11 14:02:15 -0700

    Task six

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,88 +1,89 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * strtow - Splits a string into words
- * @str: The string to be split
+ * is_separator - Checks if a character is a separator.
+ * @c: The character to check.
  *
- * Return: A pointer to an array of strings (words), or NULL if it fails
+ * Return: 1 if the character is a separator, 0 otherwise.
  */
+
+int is_separator(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The input string.
+ *
+ * Return: The number of words in the string.
+ */
+
+int count_words(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (!is_separator(str[i]) && (is_separator(str[i + 1]) || str[i + 1] == '\0'))
+	count++;
+	i++;
+	}
+
+	return (count);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: A pointer to an array of strings (words), or NULL
+ *         or if memory allocation fails.
+ */
+
 char **strtow(char *str)
 {
-	int length, word_count, in_word, i;
 	char **words;
-	int word_index, word_length, start_index, j;
+	int word_count = 0;
+	int word_length = 0;
+	int i, j, k;
 
 	if (str == NULL || *str == '\0')
-		return NULL;
+	return (NULL);
 
-	length = strlen(str);
-	word_count = 0;
-	in_word = 0;
-
-
-	for (i = 0; i < length; i++)
-	{
-		if (str[i] != ' ')
-		{
-			if (!in_word)
-			{
-				word_count++;
-				in_word = 1;
-			}
-		}
-		else
-		{
-			in_word = 0;
-		}
-	}
-
-
-	words = (char **)malloc((word_count + 1) * sizeof(char *));
+	word_count = count_words(str);
+	words = malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
-		return NULL;
+	return (NULL);
 
-	word_index = 0;
-	word_length = 0;
-	in_word = 0;
-	start_index = 0;
-
-	for (i = 0; i <= length; i++)
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
 	{
-		if (str[i] != ' ')
-		{
-			if (!in_word)
-			{
-				in_word = 1;
-				start_index = i;
-			}
-		}
-		else
-		{
-			if (in_word)
-			{
-				word_length = i - start_index;
-				words[word_index] = malloc((word_length + 1) * sizeof(char));
-				if (words[word_index] == NULL)
-				{
-
-					for (j = 0; j < word_index; j++)
-						free(words[j]);
-					free(words);
-					return NULL;
-				}
-
-				strncpy(words[word_index], str + start_index, word_length);
-				words[word_index][word_length] = '\0';
-				word_index++;
-			}
-			in_word = 0;
-		}
+	if (!is_separator(str[i]))
+	word_length++;
+	if (!is_separator(str[i]) && (is_separator(str[i + 1]) || str[i + 1] == '\0'))
+	{
+	words[j] = malloc((word_length + 1) * sizeof(char));
+	if (words[j] == NULL)
+	{
+	for (k = 0; k < j; k++)
+	free(words[k]);
+	free(words);
+	return (NULL);
 	}
+	for (k = 0; k < word_length; k++)
+	words[j][k] = str[i - word_length + 1 + k];
+	words[j][k] = '\0';
+	word_length = 0;
+	j++;
+	}
+	i++;
+	}
+	words[j] = NULL;
 
-	words[word_count] = NULL;
-
-	return words;
+	return (words);
 }
 
