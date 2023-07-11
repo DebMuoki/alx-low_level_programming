c966648c11fd299c0921648fd00f19d0204402aa
DebMuoki <ndanumuokideb@gmail.com>
2023-07-11 13:36:09 -0700

    Task six

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,60 +10,79 @@
  */
 char **strtow(char *str)
 {
-	int length;
-	int word_count;
-	int i;
+	int length, word_count, in_word, i;
 	char **words;
-	int word_index = 0;
-	int start_index = 0;
-	int word_length = 0;
-	int j;
-
+	int word_index, word_length, start_index, j;
 
 	if (str == NULL || *str == '\0')
-	return (NULL);
+		return NULL;
 
 	length = strlen(str);
 	word_count = 0;
+	in_word = 0;
 
 
 	for (i = 0; i < length; i++)
 	{
-	if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-	word_count++;
+		if (str[i] != ' ')
+		{
+			if (!in_word)
+			{
+				word_count++;
+				in_word = 1;
+			}
+		}
+		else
+		{
+			in_word = 0;
+		}
 	}
 
 
-	words = (char **) malloc((word_count + 1) * sizeof(char *));
+	words = (char **)malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
-	return (NULL);
+		return NULL;
+
+	word_index = 0;
+	word_length = 0;
+	in_word = 0;
+	start_index = 0;
 
 	for (i = 0; i <= length; i++)
 	{
-	if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-	start_index = i;
+		if (str[i] != ' ')
+		{
+			if (!in_word)
+			{
+				in_word = 1;
+				start_index = i;
+			}
+		}
+		else
+		{
+			if (in_word)
+			{
+				word_length = i - start_index;
+				words[word_index] = malloc((word_length + 1) * sizeof(char));
+				if (words[word_index] == NULL)
+				{
 
-	if ((str[i] == ' ' || str[i] == '\0') && (i > 0 && str[i - 1] != ' '))
-	{
-	word_length = i - start_index;
-	words[word_index] = malloc((word_length + 1) * sizeof(char));
-	if (words[word_index] == NULL)
-	{
+					for (j = 0; j < word_index; j++)
+						free(words[j]);
+					free(words);
+					return NULL;
+				}
 
-	for (j = 0; j < word_index; j++)
-	free(words[j]);
-	free(words);
-	return (NULL);
-	}
-
-	strncpy(words[word_index], str + start_index, word_length);
-	words[word_index][word_length] = '\0';
-	word_index++;
-	}
+				strncpy(words[word_index], str + start_index, word_length);
+				words[word_index][word_length] = '\0';
+				word_index++;
+			}
+			in_word = 0;
+		}
 	}
 
 	words[word_count] = NULL;
 
-	return (words);
+	return words;
 }
 
