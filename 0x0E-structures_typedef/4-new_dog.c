7793dbe740cb5fe564e7eb78fcd9dd8c82178d52
DebMuoki <ndanumuokideb@gmail.com>
2023-07-17 08:56:06 -0700

    Task Four

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
-#include "dog.h"
 #include <string.h>
-#include <stdio.h>
+#include "dog.h"
 
 /**
  * new_dog - Creates a new dog
@@ -15,27 +14,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr;
+	int name_length, owner_length;
 
+	/* Calculate the lengths of the name and owner strings */
+	name_length = strlen(name);
+	owner_length = strlen(owner);
+
+	/* Allocate memory for the new dog struct */
 	new_dog_ptr = malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
-		return (NULL);
-
-	new_dog_ptr->name = strdup(name);
-	if (new_dog_ptr->name == NULL)
-	{
-	free(new_dog_ptr);
 	return (NULL);
-	}
 
-	new_dog_ptr->age = age;
-
-	new_dog_ptr->owner = strdup(owner);
-	if (new_dog_ptr->owner == NULL)
+	/* Allocate memory for the name and owner strings */
+	new_dog_ptr->name = malloc((name_length + 1) * sizeof(char));
+	new_dog_ptr->owner = malloc((owner_length + 1) * sizeof(char));
+	if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
 	{
 	free(new_dog_ptr->name);
+	free(new_dog_ptr->owner);
 	free(new_dog_ptr);
 	return (NULL);
 	}
+
+	/* Copy the name and owner strings to the newly allocated memory */
+	strcpy(new_dog_ptr->name, name);
+	strcpy(new_dog_ptr->owner, owner);
+
+	/* Assign the age */
+	new_dog_ptr->age = age;
 
 	return (new_dog_ptr);
 }
