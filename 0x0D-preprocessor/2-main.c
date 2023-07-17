6f13363829b2f5d050995d21a180b4ffffce43f2
DebMuoki <ndanumuokideb@gmail.com>
2023-07-17 12:12:35 -0700

    Task two

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: return value is zero on success
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
