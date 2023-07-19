e4414f1343128f383f8df6f225d5ae3da7447cee
DebMuoki <ndanumuokideb@gmail.com>
2023-07-19 05:11:50 -0700

    Task three

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference between two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Difference between a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division result of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Division result of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the modulo of two numbers.
+ * @a: First number.
+ * @b: Second number.
+ *
+ * Return: Modulo of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
