2d359d13656981409e692024c01e040d9e9dd2df
DebMuoki <ndanumuokideb@gmail.com>
2023-07-19 07:50:18 -0700

    Task four

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,45 +2,43 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the opcodes of its own main function.
- * @argc: The number of arguments.
- * @argv: An array of pointers to the arguments.
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i, num_bytes;
-	char *main_addr;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
-	printf("Error\n");
-	exit(1);
+		printf("Error\n");
+		exit(1);
 	}
 
+	bytes = atoi(argv[1]);
 
-	num_bytes = atoi(argv[1]);
-
-	if  (num_bytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	main_addr = (char *)main;
+	arr = (char *)main;
 
-	for (i = 0; i < num_bytes; i++)
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == num_bytes - 1)
+		if (i == bytes - 1)
 		{
-			printf("%02hhx\n", main_addr[i]);
+			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", main_addr[i]);
+		printf("%02hhx ", arr[i]);
 	}
-
-
 	return (0);
 }
+
 
