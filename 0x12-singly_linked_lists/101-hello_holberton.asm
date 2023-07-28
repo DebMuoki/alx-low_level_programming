dbcaf31c2d3bfe376b5770ce111f94ef9300fb29
DebMuoki <ndanumuokideb@gmail.com>
2023-07-28 00:55:32 -0700

    Task Six

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
-section .data
-    format: db "Hello, Holberton\n", 0
-
-section .text
-    global main
-    extern printf
-
+lobal   main
+	  extern    printf
 main:
-    mov edi, format
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
 
