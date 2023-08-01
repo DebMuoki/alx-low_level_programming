92065fb1ee4f8b0c078297cdbcb9a4444f229754
DebMuoki <ndanumuokideb@gmail.com>
2023-08-01 10:53:38 -0700

    Task thirteen

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: A double pointer to the head of the listint_t list.
+ *
+ * Return: The size of the list that was free'd.
+ * The function sets the head to NULL.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nodes, index;
+	listint_t *current, *next;
+
+	nodes = looped_listint_len(*h);
+
+	if (nodes == 0)
+	{
+		current = *h;
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current);
+			current = next;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			current = *h;
+			*h = current->next;
+			free(current);
+		}
+
+		*h = NULL;
+	}
+
+	return (nodes);
+}
+
