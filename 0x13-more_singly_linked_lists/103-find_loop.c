e48ddf9b9fefdd41939b85faede6e3e35ccd6420
DebMuoki <ndanumuokideb@gmail.com>
2023-08-01 11:45:12 -0700

    Task fourteen

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,39 +1,34 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the loop in a linked list.
- * @head: A pointer to the head of the list.
- *
- * Return: The address of the node where the loop starts,
- * or NULL if there is no loop.
+ * find_listint_loop - Function that finds the loop in a linked list.
+ * @head: This is a pointer to the head of the list.
+ * Return: address of the node where the loop starts,or NULLif there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow_ptr = head, *fast_ptr = head;
+	listint_t *slow_PTR = head, *fast_PTR = head;
 
-	while (slow_ptr && fast_ptr && fast_ptr->next)
+	while (slow_PTR && fast_PTR && fast_PTR->next)
 	{
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
+		slow_PTR = slow_PTR->next;
+		fast_PTR = fast_PTR->next->next;
 
-		if (slow_ptr == fast_ptr)
+		if (slow_PTR == fast_PTR)
 		{
-			/* Loop detected, reset slow_ptr to head */
-			slow_ptr = head;
+			slow_PTR = head;
 
-			/* Move slow_ptr and fast_ptr until they meet again */
-			while (slow_ptr != fast_ptr)
+
+			while (slow_PTR != fast_PTR)
 			{
-				slow_ptr = slow_ptr->next;
-				fast_ptr = fast_ptr->next;
+				slow_PTR = slow_PTR->next;
+				fast_PTR = fast_PTR->next;
 			}
 
-			/* Return the address of the node where the loop starts */
-			return (slow_ptr);
+
+			return (slow_PTR);
 		}
 	}
-
-	/* If no loop is found, return NULL */
 	return (NULL);
 }
 
