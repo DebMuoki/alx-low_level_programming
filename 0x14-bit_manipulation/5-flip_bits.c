556ee0926ca33ab7aa822de1d8e40017715734a0
DebMuoki <ndanumuokideb@gmail.com>
2023-08-31 10:38:39 -0700

    Task five

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,10 +2,8 @@
 
 /**
  * flip_bits - Returns the number of bits
- * needed to flip to get from one number to another.
  * @n: The first number.
  * @m: The second number.
- *
  * Return: The number of bits to flip.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
