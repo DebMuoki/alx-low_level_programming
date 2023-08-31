99a85dc3b68d3d81b0271dc5b30e50b9f69e5a3a
DebMuoki <ndanumuokideb@gmail.com>
2023-08-31 07:55:38 -0700

    Changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,8 +17,8 @@ unsigned int binary_to_uint(const char *b)
 
 	for (; *b != '\0'; b++)
 	{
-	if (*b != '0' && *b != '1')
-		return (0);
+		if (*b != '0' && *b != '1')
+			return (0);
 		result = result * 2 + (*b - '0');
 	}
 	return (result);
