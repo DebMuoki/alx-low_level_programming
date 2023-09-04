eb614ebcc8ca0f1e0d8ac3f3e93771f17a0c8f50
DebMuoki <ndanumuokideb@gmail.com>
2023-09-04 10:58:04 -0700

    Task zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,26 +17,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read;
 	ssize_t bytes_written;
 
-	if (filename == NULL)
-		return (0);
 
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(file_descriptor);
+	if (filename == NULL)
 		return (0);
-	}
-
+	buffer = malloc(sizeof(char) * letters);
 	bytes_read = read(file_descriptor, buffer, letters);
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	free(buffer);
 	close(file_descriptor);
-
 	return (bytes_written);
 }
-
